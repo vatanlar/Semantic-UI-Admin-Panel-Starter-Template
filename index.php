b6dd168deac36acccdc5e7a1bc13c27ada93d334
Mehmet Ali VATANLAR <33216260+vatanlar@users.noreply.github.com>
2021-09-11 12:19:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/assets/semantic.min.css">
+    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="/assets/font-awesome.min.css">
+</head>
+<body>
+
+
+    <div class="ui top fixed secondary inverted mini menu" style="padding:5px 0px !important;background-color:#1d2327 !important;">
+    <div class="ui container">
+        <label class="active item"><strong>BUUB</strong>ADMIN <b> v0.1</b></label>
+        <a href="#" class="item"><i class="home icon"></i> GENEL BAKIŞ</a>
+        <a href="#" class="item"><i class="bars icon"></i> İÇERİK YÖNETİMİ</a>
+        <a href="#" class="item"><i class="folder icon"></i> DOSYA YÖNETİMİ</a>
+        <a href="#" class="item"><i class="users icon"></i> KULLANICILAR</a>
+        <div class="right menu">
+        <a href="#" class="item"><i class="wrench icon"></i> AYARLAR</a>
+            <a href="#" class="icon item"><i class="teal power icon"></i></a>
+        </div>
+    </div>
+    </div>
+
+    <div class="ui container" style="padding-top:45px;">
+        <div class="ui segment">test</div>
+    </div>
+
+    <script src="/assets/jquery.min.js"></script>
+    <script src="/assets/semantic.min.js"></script>
+</body>
+</html>
